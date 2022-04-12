8b7e7c3ab1e2c8fc5c8390cd4e7bb7e4a91aa167
Felix Kirschner <f.kirschner@intex-online.de>
2022-04-12 21:46:27 +0200

    Removes User info from gh-pages

--- gh-pages.js
+++ gh-pages.js
@@ -4,10 +4,6 @@ publish(
 	'build',
 	{
 		branch: 'gh-pages',
-		user: {
-			name: 'Chakah', // update to use your name
-			email: 'flxkirschner@gmail.com' // Update to use your email
-		},
 		dotfiles: true
 	},
 	(e) => {
