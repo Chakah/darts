672e3b3daec2e87435cb630653754793f9e98ce9
Felix Kirschner <f.kirschner@intex-online.de>
2022-04-12 21:42:49 +0200

    Updates gh-pages

--- gh-pages.js
+++ gh-pages.js
@@ -1,5 +1,16 @@
 import { publish } from 'gh-pages';
 
-publish('build', (e) => {
-	console.log('Deploy Complete!', e);
-});
+publish(
+	'build',
+	{
+		branch: 'gh-pages',
+		user: {
+			name: 'Chakah', // update to use your name
+			email: 'flxkirschner@gmail.com' // Update to use your email
+		},
+		dotfiles: true
+	},
+	(e) => {
+		console.log('Deploy Complete!', e);
+	}
+);
