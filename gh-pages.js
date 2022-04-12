076b31e5c9f122498a7c323785e27931398b6b9f
Felix Kirschner <f.kirschner@intex-online.de>
2022-04-12 21:24:26 +0200

    Changes gh-pages

--- gh-pages.js
+++ gh-pages.js
@@ -1,17 +1,5 @@
 import { publish } from 'gh-pages';
 
-publish(
-	'build',
-	{
-		branch: 'gh-pages',
-		repo: 'https://github.com/Chakah/darts.git',
-		user: {
-			name: 'Chakah',
-			email: 'flxkirschner@gmail.com'
-		},
-		dotfiles: true
-	},
-	(e) => {
-		console.log('Deploy Complete!', e);
-	}
-);
+publish('build', (e) => {
+	console.log('Deploy Complete!', e);
+});
